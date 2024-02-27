dff4ad250f8f4c740f91e58ababbde8b15ab93ba
Kuroda Mitsuhiko (黒田 光彦) <160799769+KurodaMitsuhiko@users.noreply.github.com>
2024-02-27 09:49:18 +0900

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "Imagestorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
